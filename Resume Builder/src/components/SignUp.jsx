f2c0763335e79226c1ca6912ea5b4aa4d4282635
thechildwholovesyou <ssecutities@gmail.com>
2021-09-12 18:26:38 +0530

    signUp created

--- Resume Builder/src/components/SignUp.jsx
+++ Resume Builder/src/components/SignUp.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { useHistory } from "react-router";
+import { auth } from "../firebase";
 
 let SignUp = () => {
   let history = useHistory();
+
+  let [password, setPassword] = useState("");
+  let [confirmPassword, setConfirmPassword] = useState("");
+  let [email, setEmail] = useState("");
 
   return (
     <>
@@ -15,6 +21,10 @@ let SignUp = () => {
             class="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+            }}
           />
         </div>
         <div class="mb-3">
@@ -25,6 +35,10 @@ let SignUp = () => {
             type="password"
             class="form-control"
             id="exampleInputPassword1"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.currentTarget.value);
+            }}
           />
         </div>
 
@@ -36,9 +50,22 @@ let SignUp = () => {
             type="password"
             class="form-control"
             id="exampleInputPassword1"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.currentTarget.value);
+            }}
           />
         </div>
-        <button type="submit" class="btn btn-primary m-4">
+        <button
+          type="submit"
+          class="btn btn-primary m-4"
+          onClick={(e) => {
+            e.preventDefault();
+            if (password === confirmPassword) {
+              auth.createUserWithEmailAndPassword(email, password);
+            }
+          }}
+        >
           SignUp
         </button>
 
